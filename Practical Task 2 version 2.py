64c07d37cf07ffa0736723c7ccc4eebdf898e391
krassN <krassnet@mail.ru>
2024-07-21 19:00:11 +0300

    Add files via upload

---
+++ Practical Task 2 version 2.py
@@ -0,0 +1,6 @@
+homeWork = 12
+wholeTime = 1.5
+courseName = 'Python'
+taskTime = (wholeTime / homeWork)
+print('Курс:', courseName, ',всего задач:', homeWork, ',затрачено часов:', wholeTime, ',среднее время выполнеия:',
+      taskTime, 'часа.')
---
+++ Practical Task 2.py
@@ -0,0 +1,9 @@
+homeWork = 12
+print(homeWork)
+wholeTime = 1.5
+print(wholeTime)
+courseName = 'Python'
+taskTime = (wholeTime / homeWork)
+print(taskTime)
+print('Курс:', courseName, ',всего задач:', homeWork, ',затрачено часов:', wholeTime, 'среднее время выполнеия:',
+      taskTime, 'часа.')
